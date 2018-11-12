0dc4f4411fda6385bac58d48651dbfff0f2fb453
Max Ranieri <otherranieri@gmail.com>
2018-11-11 22:36:28 -0500

    Extremely slow haikus lol

--- poet.py
+++ poet.py
@@ -3,8 +3,21 @@ import requests
 import string
 import random
 import itertools
+from nltk.corpus import cmudict
 
 class Poet:
+
+    commonbaddies = [
+    'and',
+    'of',
+    'the',
+    'as',
+    'for'
+    'a',
+    'to',
+    'like',
+    'in'
+    ]
 
     def __init__(self):
         pass
@@ -13,29 +26,33 @@ class Poet:
         target_html = requests.get(url).text
         self.soup = bs.BeautifulSoup(target_html, 'html.parser')
 
-    def makechain(self, words):
-        chain = {}
-        thruples = ((a, b, c) for a, b, c in zip(words, words[1:], words[2:]))
-        for t in thruples:
-            k, v = (t[0], t[1]), t[2]
-            try:
-                chain[k].append(v)
-            except KeyError:
-                chain[k] = [v]
-        return chain
+    def countsyl(self, words):
+        ct = 0
+        d = cmudict.dict()
+        for word in words:
+            if(word.lower() in d):
+                ct += [len(list(y for y in x if y[-1].isdigit())) for x in d[word.lower()]][0]
+        return ct
+
+    def makechain(self, words, syl):
+        rx = random.randint(0, len(words)-1)
+        line = []
+        while(self.countsyl(line) != syl or line[-1] in self.commonbaddies):
+            line.append(words[rx].lower())
+            if(self.countsyl(line) > 5):
+                line = line[1:]
+            rx+=1
+            print(line)
+        return line
 
     def getpoem(self):
         ps = [p.get_text() for p in self.soup.find_all('p')]
         text = '\n'.join(ps)
         words = text.split()
         words = [word.strip() for word in words if all([c in string.ascii_letters for c in word])]
-        chain = self.makechain(words)
-        rx = random.randint(0, len(chain.keys())-1)
-        prevkey = list(chain.keys())[rx] # root
-        re = lambda x: x[random.randint(0,len(x)-1)]# random element
-        poem = [*prevkey]
-        for i in range(18):
-            word = re(chain[prevkey])
-            prevkey = (prevkey[1], word)
-            poem.append(word)
-        return ' '.join(poem)
+        line_1 = self.makechain(words, 5)
+        line_2 = self.makechain(words, 7)
+        line_3 = self.makechain(words, 5)
+
+        poem = ' '.join(line_1 + ['|'] + line_2 + ['|'] + line_3)
+        return poem
